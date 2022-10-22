8d3657bb9a84248797fa0034d3113a1eeedb7ae5
Maxim <maximkazyankin@gmail.com>
2022-10-22 16:32:34 +0300

    Upload Cards

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Card from "./Components/Card/Card";
+import svg from "./Components/img/img";
+
+const cardsBody = {
+	title: "Card title",
+	text: `Some quick example text to build on the card title and make up the bulk of the card's content.`,
+};
+
+function App() {
+	return (
+		<div className="App container">
+			<Card cardsBody={cardsBody}>{svg}</Card>
+			<Card cardsBody={cardsBody}></Card>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/img/img.js
@@ -0,0 +1,11 @@
+let svg = (
+	<svg>
+		<title>Placeholder</title>
+		<rect width="100%" height="100%" fill="#868e96"></rect>
+		<text x="40%" y="50%" fill="#dee2e6">
+			Image cap
+		</text>
+	</svg>
+);
+
+export default svg;
